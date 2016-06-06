2914a28006e59642ddf4a03e867a0465f11b7f35
fuegos1981 <fuegos1981@gmail.com>
2016-06-06 21:11:54 +0300

    change

--- ChangeWord/src/ua/fuego_2000/ValInt/Main.java
+++ ChangeWord/src/ua/fuego_2000/ValInt/Main.java
@@ -5,35 +5,32 @@ import java.util.regex.Pattern;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		String s = "-2147483648";
 		String sRes;
 		boolean isMinus = false;
-		if (s.startsWith("-")){
+		if (s.startsWith("-")) {
 			sRes = s.substring(1);
 			isMinus = true;
-		}
-		else{
+		} else {
 			sRes = s;
 		}
-		if (Pattern.matches("\\D", sRes)){
+		if (Pattern.matches("\\D", sRes)) {
 			throw new NumberFormatException();
-		}
-		else{
+		} else {
 			char[] ch = sRes.toCharArray();
-			int result=0;
+			int result = 0;
 			for (int i = 0; i < ch.length; i++) {
-				
-				int res = (int)ch[i]-48;
-				if (isMinus){
-					result = (int) (result-res*Math.pow(10, ch.length-i-1));
+
+				int res = (int) ch[i] - 48;
+				if (isMinus) {
+					result = (int) (result - res * Math.pow(10, ch.length - i - 1));
+				} else {
+					result = (int) (result + res * Math.pow(10, ch.length - i - 1));
 				}
-				else{
-					result = (int) (result+res*Math.pow(10, ch.length-i-1));
-				}
-				
+
 			}
-			if (!s.equals(""+result)){
+			if (!s.equals("" + result)) {
 				throw new NumberFormatException();
 			}
 			System.out.println(result);
